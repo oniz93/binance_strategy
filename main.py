6c79e1a3760284a3ef20d2a3b095a64fc18dd266
Teo <teo.miscia@gmail.com>
2021-06-10 18:31:30 +0200

    Update

--- main.py
+++ main.py
@@ -7,6 +7,8 @@ import pandas_ta as ta
 import asyncio
 import time
 import requests
+from datetime import datetime
+from multiprocessing import Process
 # This is a sample Python script.
 
 # Press ⌃R to execute it or replace it with your code.
@@ -15,12 +17,48 @@ import time
 
 from binance import ThreadedWebsocketManager
 
+def timeframeToSeconds(tf):
+    if tf == '1m':
+        return 60
+    elif tf == '3m':
+        return 60 * 3
+    elif tf == '5m':
+        return 60 * 5
+    elif tf == '15m':
+        return 60 * 15
+    elif tf == '30m':
+        return 60 * 30
+    elif tf == '1h':
+        return 60 * 60
+    elif tf == '2h':
+        return 60 * 60 * 2
+    elif tf == '4h':
+        return 60 * 60 * 4
+    elif tf == '6h':
+        return 60 * 60 * 6
+    elif tf == '8h':
+        return 60 * 60 * 8
+    elif tf == '12h':
+        return 60 * 60 * 12
+    elif tf == '1d':
+        return 60 * 60 * 24
+    elif tf == '3d':
+        return 60 * 60 * 24 * 3
+    elif tf == '1w':
+        return 60 * 60 * 24 * 7
+    else:
+        return 60 * 60
+
+
 cwd = os.getcwd()
 configfile = open(cwd + "/cfg.json", 'r')
 config = json.loads(configfile.read())
 configfile.close()
 api_key = config['binance_key']
 api_secret = config['binance_secret']
+client = Client(api_key, api_secret)
+timeframe = config['timeframe']
+secondsTf = timeframeToSeconds(timeframe)
 
 def signToTick():
     try:
@@ -41,50 +79,97 @@ def signToTick():
         print("control-c")
         twm.stop()
 
+def check_coin(symbol):
+    print("Start %s" % (symbol,))
+    while True:
+        timestamp = client._get_earliest_valid_timestamp(symbol, timeframe)
+        wait_next = timestamp + secondsTf
+        #bars = client.get_historical_klines('BTCUSDT', '4h', timestamp, limit=50)
+        response = requests.get(
+            url="https://api.binance.com/api/v3/klines",
+            params={
+                "symbol": symbol,
+                "interval": timeframe,
+                "limit": "50",
+            },
+            headers={
+                "Content-Type": "application/json",
+            },
+        )
+        bars = json.loads(response.content)
+        df = pd.DataFrame(bars, columns=['date', 'open', 'high', 'low', 'close','volume','close_time','quote_volume','n_trades','taker_buy_quote','taker_buy_asset','ignore'])
+        df[['open','high','low','close']] = df[['open','high','low','close']].apply(pd.to_numeric)
+        df.set_index('date', inplace=True)
+
+        df.ta.ema(close='close', length=4, append=True)
+        df.ta.ema(close='close', length=9, append=True)
+        df.ta.ema(close='close', length=40, append=True)
+        df.ta.ema(close='low', length=40, append=True, suffix='low')
+        df.ta.ema(close='high', length=40, append=True, suffix='high')
+
+        check_ticks = df[-2:-1]
+        c_t = 0
+        c_l = 0
+        c_ct = 0
+        control_ticks = df[-21:-1]
+        for index, tick in control_ticks.iterrows():
+            if tick['EMA_40_high'] > tick['EMA_9'] and tick['EMA_9'] > tick['EMA_40_low']:
+                c_l = c_l +1
+            if tick['EMA_9'] > tick['EMA_40_high']:
+                c_t = c_t +1
+            if tick['EMA_9'] < tick['EMA_40_low']:
+                c_ct = c_ct +1
+
+        for index, check_tick in check_ticks.iterrows():
+            if check_tick['low'] < check_tick['EMA_4'] and check_tick['low'] < check_tick['EMA_9'] and check_tick['low'] < check_tick['EMA_40'] and c_t >= 8 and c_l < 10:
+                take_profit = check_tick['close']-check_tick['open']+check_tick['close']
+                stop_loss = check_tick['low'] - (check_tick['high']-check_tick['low'])
+                response = requests.get(
+                    url="https://api.binance.com/api/v3/avgPrice",
+                    params={
+                        "symbol": symbol,
+                    },
+                    headers={
+                        "Content-Type": "application/json",
+                    },
+                )
+                price = json.loads(response.content)
+                price = float(price['price'])
+                price = price + price*0.0001
+                if price < take_profit:
+                    est_perc = take_profit/price
+                    current_time = (datetime.utcfromtimestamp(time.time()).strftime('%Y-%m-%d %H:%M:%S'))
+                    file_currency = open(cwd + "/" + symbol + ".log", 'a')
+                    file_currency.write("Date %s - Price: %f - Take profit: %f - Stop loss: %f - Gain: %f - T = %d - L = %d - CT = %d\n" % (str(current_time), price, take_profit, stop_loss, est_perc,c_t,c_l, c_ct,))
+                    file_currency.close()
+
+        while time.time() < wait_next:
+            time.sleep(1)
+
 def main():
-    client = Client(api_key, api_secret)
-    timestamp = client._get_earliest_valid_timestamp('BTCUSDT', '4h')
-    #bars = client.get_historical_klines('BTCUSDT', '4h', timestamp, limit=50)
-    response = requests.get(
-        url="https://api.binance.com/api/v3/klines",
-        params={
-            "symbol": "BTCUSDT",
-            "interval": "4h",
-            "limit": "50",
-        },
-        headers={
-            "Content-Type": "application/json",
-        },
-    )
-    bars = json.loads(response.content)
-    df = pd.DataFrame(bars, columns=['date', 'open', 'high', 'low', 'close','volume','close_time','quote_volume','n_trades','taker_buy_quote','taker_buy_asset','ignore'])
-    df[['open','high','low','close']] = df[['open','high','low','close']].apply(pd.to_numeric)
-    df.set_index('date', inplace=True)
+    workers = list()
+    try:
+        response = requests.get(
+            url="https://api.binance.com/api/v3/exchangeInfo",
+            params={
+            },
+            headers={
+                "Content-Type": "application/json",
+            },
+        )
+        coins = json.loads(response.content)
+        for symbol in coins['symbols']:
+            if symbol['quoteAsset'] == 'USDT':
+                p = Process(target=check_coin, args=(symbol['symbol'],))
+                p.start()
+                workers.append(p)
+                time.sleep(0.01)
 
-    df.ta.ema(close='close', length=4, append=True)
-    df.ta.ema(close='close', length=9, append=True)
-    df.ta.ema(close='close', length=40, append=True)
-    df.ta.ema(close='low', length=40, append=True, suffix='low')
-    df.ta.ema(close='high', length=40, append=True, suffix='high')
-
-    check_ticks = df[-2:-1]
-    c_t = 0
-    c_l = 0
-    c_ct = 0
-    control_ticks = df[-21:-1]
-    for index, tick in control_ticks.iterrows():
-        if tick['EMA_40_high'] > tick['EMA_9'] and tick['EMA_9'] > tick['EMA_40_low']:
-            c_l = c_l +1
-        if tick['EMA_9'] > tick['EMA_40_high']:
-            c_t = c_t +1
-        if tick['EMA_9'] < tick['EMA_40_low']:
-            c_ct = c_ct +1
-
-    for index, check_tick in check_ticks.iterrows():
-        if check_tick['low'] < check_tick['EMA_4'] and check_tick['low'] < check_tick['EMA_9'] and check_tick['low'] < check_tick['EMA_40']:
-            print("trovato")
-
-    print("T = %d - L = %d - CT = %d" % (c_t,c_l, c_ct))
+    except KeyboardInterrupt:
+        print("control-c")
+        for p in workers:
+            p.terminate()
+            p.join()
 
 if __name__ == "__main__":
    main()
